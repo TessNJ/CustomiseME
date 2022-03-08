fa25417faa5d8e33db8a3b0e136596d3ec9faa5a
Giorgia Meton <gior0166@stud.kea.dk>
2022-03-08 10:19:51 +0100

    basic burger menu function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//burger menu function
+
+//Event listener
+document.querySelector(".menu").addEventListener("click", burgerMenu);
+//Toggle between showing and hiding the burger menu
+function burgerMenu() {
+  var x = document.querySelector("header nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
