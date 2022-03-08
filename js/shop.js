93f2eecbedfb8ebe2cfe596086b6362a2ee604e8
Tess <tere0506@stud.kea.dk>
2022-03-08 10:55:27 +0100

    filtering works!!!!!

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,10 @@
 /*Getting Databse */
-
-let url = "https://kea21a2sem-d273.restdb.io/rest/t7-soap";
-
 const urlParams = new URLSearchParams(window.location.search);
 console.log("URL params: " + urlParams);
 const category = urlParams.get("Category");
 console.log("category param: " + category);
+
+let url = "https://kea21a2sem-d273.restdb.io/rest/t7-soap?Category=";
 
 const options = {
   headers: {
@@ -14,21 +13,26 @@ const options = {
 };
 
 function showSoaps(soaps) {
-  console.log("Success!");
-  //Grab template
-  const template = document.querySelector("#shop_template").content;
-  //Clone it
-  const copy = template.cloneNode(true);
-  //Change content
-  copy.querySelector(".shop_name").textContent = soaps.Name;
-  copy.querySelector(".shop_price").textContent = `${soaps.Price} DKK`;
-  copy.querySelector("img").src = soaps.img_src;
-  copy.querySelector("img").alt = soaps.img_alt;
-  copy.querySelector("a").setAttribute("href", `product.html?id=${soaps._id}`);
-  //Grab parent
-  const parent = document.querySelector(".dynamic_content");
-  //Append Child
-  parent.appendChild(copy);
+  console.log(soaps.Category);
+  if (category == "all" || soaps.Category == category) {
+    console.log("Success!");
+    //Grab template
+    const template = document.querySelector("#shop_template").content;
+    //Clone it
+    const copy = template.cloneNode(true);
+    //Change content
+    copy.querySelector(".shop_name").textContent = soaps.Name;
+    copy.querySelector(".shop_price").textContent = `${soaps.Price} DKK`;
+    copy.querySelector("img").src = soaps.img_src;
+    copy.querySelector("img").alt = soaps.img_alt;
+    copy
+      .querySelector("a")
+      .setAttribute("href", `product.html?id=${soaps._id}`);
+    //Grab parent
+    const parent = document.querySelector(".dynamic_content");
+    //Append Child
+    parent.appendChild(copy);
+  }
 }
 
 fetch(url, options)
