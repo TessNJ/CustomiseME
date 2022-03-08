4a6b24610baf8d87408b3d07b0a9422b91874f2a
Tess <tere0506@stud.kea.dk>
2022-03-08 14:27:59 +0100

    failed

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,10 @@
+/*Preloader */
+$(window).load(function () {
+  // makes sure the whole site is loaded
+  $("#status").fadeOut(); // will first fade out the loading animation
+  $("#preloader").delay(50).fadeOut(100); // will fade out the white DIV that covers the website.
+  $("body").delay(50).css({ overflow: "visible" });
+});
 /*Getting Databse */
 const urlParams = new URLSearchParams(window.location.search);
 console.log("URL params: " + urlParams);
