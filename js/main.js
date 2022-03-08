5ad7742608c085778457622d42952190bace24cf
nynnerasch <nynne.andersen@yahoo.dk>
2022-03-08 12:38:18 +0100

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-let url =
-  "https://kea21a2sem-d273.restdb.io/rest/t7-soap/6221d597b55385400000e99b";
-
 const urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
 // const id = urlParams.get("_id");
+let url =
+  "https://kea21a2sem-d273.restdb.io/rest/t7-soap/6221d701b55385400000e9bc";
 
 const option = {
   headers: {
@@ -21,4 +21,15 @@ fetch(url, option)
 function handleProduct(soaps) {
   document.querySelector(".single_img").src = soaps.img_src;
   document.querySelector(".single_img").alt = soaps.img_alt;
+  document.querySelector(".singleProduct_name").textContent = soaps.Name;
+  document.querySelector(
+    ".singleProduct_price"
+  ).textContent = `DKK ${soaps.Price}`;
+  document.querySelector(".singleProduct_oils").textContent =
+    soaps.Essential_Oils;
+  document.querySelector(".singleProduct_colour").textContent = soaps.Colour;
+  document.querySelector(".singleProduct_shape").textContent = soaps.Shape;
+  document.querySelector(".singleProduct_weight").textContent = soaps.Weight;
+  document.querySelector(".singleProduct_availability").textContent =
+    soaps.Availability;
 }
